df5f22d2f0a0ff3eaf12536e1c45da04ed43ee41
Maximo10 <birhanff@gmail.com>
2026-04-08 17:14:02 +0200

    Nuevo

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const app = express();
 app.use(express.json());
 
 async function main() {
-    const DB_URI = process.env.MONGO_URI || "mongodb://localhost:27017/mydatabase";
+    try{
+        const DB_URI = process.env.MONGO_URI || "mongodb://localhost:27017/mydatabase";
         await mongoose.connect(DB_URI);
         console.log("Conexion a la base de datos exitosa");
 
@@ -32,7 +33,7 @@ async function main() {
         }
 
         app.get("/",async (req, res) => {
-            res.json({message: "Api con Webhook Funcionando Correctamente"});
+            res.json({message: "Api con Webhook funcionando correctamente"});
         });
 
         //emdpoint Usuarios 
@@ -74,7 +75,8 @@ async function main() {
             } catch (error) {
                 res.status(400).json({ message: error.message });
             }
-        });                
+        });
+
         //endpoint Grupos
         app.get("/grupos", async (req, res) => {
             const grupos = await grupo.find();
